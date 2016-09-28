a510d4303c6919611a3ed4e0326bee1a36d8f5fa
forwk1990 <forwk1990@gmail.com>
2016-09-28 10:47:20 +0800

    property exchange initialize

---
+++ PropertyExchange/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PropertyExchange
+//
+//  Created by itachi on 16/9/28.
+//  Copyright © 2016年 com.itachi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PropertyExchange/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PropertyExchange
+//
+//  Created by itachi on 16/9/28.
+//  Copyright © 2016年 com.itachi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
